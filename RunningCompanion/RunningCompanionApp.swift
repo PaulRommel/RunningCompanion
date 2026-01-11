80a215c62878581661fc90e527c6aaee4c88f53c
Paul <paulrommel87@gmail.com>
2026-01-11 16:39:26 +0300

    Initial Commit

---
+++ RunningCompanion/RunningCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningCompanionApp.swift
+//  RunningCompanion
+//
+//  Created by Попов Павел on 11.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct RunningCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
